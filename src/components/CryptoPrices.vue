63c93cb919f8381355967299c889315acca29cca
ayoobnazzz <ayoobnaz@hotmail.com>
2024-06-05 02:38:26 -0500

    main table data logic updated

--- src/components/CryptoPrices.vue
+++ src/components/CryptoPrices.vue
@@ -19,6 +19,13 @@
           </tr>
         </tbody>
       </table>
+      <div v-if="loading" class="loading">Loading...</div>
+      <div v-if="error" class="error">{{ error }}</div>
+      <div class="pagination">
+        <button @click="prevPage" :disabled="page === 1">Previous</button>
+        <span>Page {{ page }}</span>
+        <button @click="nextPage">Next</button>
+      </div>
     </div>
   </template>
   
@@ -28,28 +35,46 @@
   export default {
     data() {
       return {
-        cryptocurrencies: []
+        cryptocurrencies: [],
+        loading: false,
+        error: null,
+        page: 1
       };
     },
     methods: {
       async fetchCryptoData() {
+        this.loading = true;
         try {
           const response = await axios.get('https://api.coingecko.com/api/v3/coins/markets', {
             params: {
               vs_currency: 'usd',
               order: 'market_cap_desc',
               per_page: 10,
-              page: 1,
+              page: this.page,
               sparkline: false
             }
           });
           this.cryptocurrencies = response.data;
+          this.error = null;
         } catch (error) {
+          this.error = 'Error fetching cryptocurrency data.';
           console.error('Error fetching cryptocurrency data:', error);
+        } finally {
+          this.loading = false;
         }
       },
       goToDetail(id) {
         this.$router.push({ name: 'Detail', params: { id } });
+      },
+      nextPage() {
+        this.page++;
+        this.fetchCryptoData();
+      },
+      prevPage() {
+        if (this.page > 1) {
+          this.page--;
+          this.fetchCryptoData();
+        }
       }
     },
     created() {
@@ -80,6 +105,27 @@
   
   tbody tr:hover {
     background-color: #f5f5f5;
+  }
+  
+  .loading {
+    text-align: center;
+    margin: 20px 0;
+  }
+  
+  .error {
+    color: red;
+    text-align: center;
+    margin: 20px 0;
+  }
+  
+  .pagination {
+    display: flex;
+    justify-content: center;
+    margin: 20px 0;
+  }
+  
+  .pagination button {
+    margin: 0 5px;
   }
   </style>
   
